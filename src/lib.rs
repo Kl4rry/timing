7741d97e60b2c9f3070a2d080b90767a4a801a77
BuildTools <unconfigured@null.spigotmc.org>
2020-11-21 18:48:23 +0100

    removed test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 use std::time::{Instant, Duration};
 use std::ops::Drop;
-use std::time;
-use std::thread;
 
 pub struct Timer {
     start: Instant,
@@ -23,12 +21,5 @@ impl Drop for Timer {
     fn drop(&mut self) {
         println!("{}", self.start.elapsed().as_millis());
     }
-}
-
-#[test]
-fn it_works() {
-    let timer = Timer::start();
-    thread::sleep(time::Duration::from_millis(10));
-    assert_eq!(timer.stop().as_millis(), 10);
 }
 
